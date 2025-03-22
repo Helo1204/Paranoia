6c94cb8eddb07a46c58eaaf3a7eaed4c7ed76fe2
Alfghard <alfghardg@gmail.com>
2025-03-22 14:57:37 +0100

    Paranoia Effect

--- Assets/Scripts/Player/ParanoiaEffect.cs
+++ Assets/Scripts/Player/ParanoiaEffect.cs
@@ -3,16 +3,41 @@ using UnityEngine;
 public class ParanoiaEffect : MonoBehaviour
 {
     public ParanoiaBar ParanoiaBar;
-    public float ParanoiaAmount;
+    float ParanoiaAmount;
+    public Camera PlayerCamera;
+    float InitialFOV = 90f;
+    public float MaxFOV = 100f;
+    public float MinFOV = 70f;
+
+    float MaxParanoia;
+    float MinParanoia;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        MinParanoia = ParanoiaBar.minimum;
+        MaxParanoia = ParanoiaBar.maximum;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         ParanoiaAmount = ParanoiaBar.GetCurrentFill();
+        
+        PlayerCamera.fieldOfView = FOVfunction(ParanoiaAmount);
+        Debug.Log(PlayerCamera.fieldOfView);
+    }
+
+    void ResetFOV()
+    {
+        PlayerCamera.fieldOfView = InitialFOV;
+    }
+
+    private float FOVfunction(float ParanoiaAmount)
+    {
+        float percentile = ParanoiaAmount / (MaxParanoia-MinParanoia);
+        return (percentile*MinFOV)+((1-percentile)*MaxFOV);
     }
 }
