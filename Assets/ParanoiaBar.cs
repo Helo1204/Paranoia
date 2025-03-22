1b0d20e597c9d4f3c78b9f474130010a110b037d
Alfghard <alfghardg@gmail.com>
2025-03-22 01:08:49 +0100

    paranoia bar

---
+++ Assets/ParanoiaBar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[ExecuteInEditMode()]
+public class ParanoiaBar : MonoBehaviour
+{
+    public int maximum;
+    public int minimum;
+    public int current;
+    public Image mask;
+    //public Image fill;
+    //public Color color;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillAmount = currentOffset / maximumOffset;
+        //float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+
+        //fill.color = color;
+    }
+}
