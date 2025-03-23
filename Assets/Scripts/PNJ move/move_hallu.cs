aa6c40b1ac07838f016a4594d0388310d815c87c
Helo1204 <heloise.havy@gmail.com>
2025-03-23 12:14:53 +0100

    couteau texture

--- Assets/Scripts/PNJ move/move_hallu.cs
+++ Assets/Scripts/PNJ move/move_hallu.cs
@@ -7,7 +7,6 @@ public class move_hallu : MonoBehaviour
 {
     [SerializeField] float speed;
     
-    private Vector3 destinationPosition;
     private Vector3 destinationFinal;
     private NavMeshAgent agent;
     private Person person;
@@ -24,18 +23,11 @@ public class move_hallu : MonoBehaviour
 
         destinationFinal = EnemySpawner.Main.PlayerSpawnTransform.position;
         destinationFinal.z = transform.position.z;
-        destinationPosition = destinationFinal;
 
         agent.speed = speed;
         agent.SetDestination(destinationFinal);
-        if(Random.Range(0, 100) < LevelManager.Main.Difficulty * 10) { wantTalk = true; }
 
-        offset = new Vector3(-2,0,0);
-        destinationPosition = player.transform.position+offset ;
-        if (wantTalk)
-        {
-            destinationPosition = player.transform.position + offset;
-        }
+
     }
     private void Update()
     {
@@ -44,30 +36,17 @@ public class move_hallu : MonoBehaviour
         {
             return;
         }
-        agent.SetDestination(destinationPosition);
-        if (wantTalk)
-        {
-            Debug.Log("talk");
-            destinationPosition = player.transform.position + offset;
-        }
+        agent.SetDestination(destinationFinal);
+
 
         if (agent.remainingDistance <= agent.stoppingDistance)
         {   
-            if(wantTalk)
-            {
-                StartCoroutine(Talk());
-            }
+
             if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
             {
                 Destroy(gameObject);
             }
         }
 
-    }
-    private IEnumerator Talk()
-    {
-        yield return new WaitForSeconds(2);
-        wantTalk= false;
-        destinationPosition = destinationFinal;
-    }
+
 }
