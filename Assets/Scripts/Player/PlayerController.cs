3d6770f38f257f24fe098094829ff8b828355fb7
tornac1234 <24827220+tornac1234@users.noreply.github.com>
2025-03-23 14:59:04 +0100

    réglé un bug probablement ...

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -58,8 +58,8 @@ public class PlayerController : MonoBehaviour
             else
             {
                 MenuSystem.Main.Hide();
-                Time.timeScale = 1f;
                 SetControlsEnabled(true);
+                Time.timeScale = 1f;
             }
         }
 
@@ -119,8 +119,7 @@ public class PlayerController : MonoBehaviour
 
     public void SlowlyFaceUpwards(float deltaT)
     {
-        float newX = Mathf.Lerp(CameraTransform.localRotation.eulerAngles.x, 5f, deltaT);
-        CameraTransform.localRotation = Quaternion.Euler(newX, 0, 0);
+        CameraTransform.localRotation = Quaternion.Slerp(CameraTransform.localRotation, Quaternion.identity, deltaT);
     }
 
     public void LockCursor(bool state)
@@ -132,7 +131,6 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
